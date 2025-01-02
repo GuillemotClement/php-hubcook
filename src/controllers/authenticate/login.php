8ebdf3ed277dfd8fa9387c8871e1f1e0adc6ac98
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-02 19:54:25 +0100

    Add login with db

--- src/controllers/authenticate/login.php
+++ src/controllers/authenticate/login.php
@@ -1,18 +1,22 @@
 <?php
-
+p($_SESSION);
 //p($_REQUEST);
 //p($_SERVER);
-$dataUser = [
-  "username" => "gizmo",
-  "password" => "123456"
-];
+//$dataUser = [
+//  "username" => "gizmo",
+//  "password" => "123456"
+//];
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
   $username = $_REQUEST['username'];
   $password = $_REQUEST['password'];
 
-  if($username === $dataUser['username']){
-    if($password === $dataUser['password']){
+  $sql = "SELECT * FROM user WHERE user_username LIKE :username";
+  $params = [":username" => $username];
+  $user = $db->fetchOne($sql, $params);
+  p($user);
+  if($username === $user['user_username']){
+    if($password === $user['user_password']){
       $_SESSION['username'] = $username;
     }else{
       header("Location: /");
