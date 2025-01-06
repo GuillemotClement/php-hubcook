d8752b60de42fd3c26cf1263f6a743e995141b31
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-06 18:44:56 +0100

    Add .env example

--- public/index.php
+++ public/index.php
@@ -1,50 +1,45 @@
 <?php
 
+use Dotenv\Dotenv;
 use Hubcook\Core\Database;
 use Hubcook\Core\Router;
 use Hubcook\Core\Utils;
 
 session_start();
-const BASE_PATH = __DIR__ . '/../';
-require BASE_PATH."vendor/autoload.php";
-require BASE_PATH."src/Core/functions.php";
+const BASE_PATH = __DIR__ . "/../";
+require BASE_PATH . "vendor/autoload.php";
 
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
+Utils::printAndDie($_ENV);
+
+require BASE_PATH . "src/Core/functions.php";
 require BASE_PATH . "src/utils/routing.php";
-
 
 $router = new Router();
 
-
-require BASE_PATH."src/Core/routes.php";
+require BASE_PATH . "src/Core/routes.php";
 
 //stocker les variable dans un fichier env
 $db = new Database(
-  "pgsql:host=localhost;port=5432;dbname=hubcook",
-  "clement",
-  ""
+    "pgsql:host=localhost;port=5432;dbname=hubcook",
+    "clement",
+    ""
 );
 
-$sql = "SELECT * FROM recipe";
+// Utils::printValue($recipe);
 
-$recipe = $db->fetchAll($sql);
-Utils::printValue($recipe);
-
-
-Utils::printValue($db);
+// Utils::printValue($db);
 //Utils::printAndDie($_SERVER);
 
-$url = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
+$url = $_SERVER["REQUEST_URI"];
+$method = $_SERVER["REQUEST_METHOD"];
 $router->routeTo($url, $method);
-
 
 //on ajoute une nouvelle route et on écrase l'ancien tableau contenant toutes les routes
 // $routes = addRoute($routes, '/recipe', 'recipe/listRecip', 'GET');
 //$routes = routeGet("/login", 'authenticate/login', $routes);
 
-
-
 // p($_SERVER['REQUEST_METHOD']);
 //routeTo($_SERVER['REQUEST_URI'], $routes, $_SERVER['REQUEST_METHOD']);
-
